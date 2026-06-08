b04675847da00433b0eb1828fb7dca174b12321c
Maheswaran091 <mahesrko87@gmail.com>
2026-06-09 02:30:55 +0530

    Urls set

--- backend/coffeeshop_backend/urls.py
+++ backend/coffeeshop_backend/urls.py
@@ -3,7 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Coffee Shop Backend Running")
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
