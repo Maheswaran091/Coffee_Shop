037dae8e16af4a749e747c257084bb1dab5740a6
Maheswaran091 <mahesrko87@gmail.com>
2026-07-15 14:54:17 +0530

    replace

--- backend/coffeeshop_backend/settings.py
+++ backend/coffeeshop_backend/settings.py
@@ -68,21 +68,22 @@ WSGI_APPLICATION = 'coffeeshop_backend.wsgi.application'
 
 # ===================== DATABASE =====================
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT", "4000"),
-        "OPTIONS": {
-            "ssl": {
-                "ca": BASE_DIR / "certs" / "isrgrootx1.pem",
-            }
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-}
 
 # ===================== PASSWORD VALIDATION =====================
 
